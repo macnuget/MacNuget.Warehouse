e68aa30c7ee957454f4d9450c26379616fd23706
Stefan Mihalache <ste.mihalache@gmail.com>
2021-12-02 17:25:23 +0100

    add dtos

---
+++ MacNuget.Warehouse.Domain/Dto/RefillDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacNuget.Warehouse.Domain.Dto
+{
+    public class RefillDto
+    {
+        public int Id { get; set; }
+        public string SupplierId { get; set; }
+        public DateTime ArriveDate { get; set; }
+        public int? ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
